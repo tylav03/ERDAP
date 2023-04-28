b9d87ec4dab3fff8d581c45d99980797bb88ec82
tylav03 <tylerlavaway@gmail.com>
2023-04-27 20:19:17 -0400

    Deleted temp file test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-num = int(BeautifulSoup(requests.get(url = 'https://bewell.ese.syr.edu/FacilityOccupancy').text, "html.parser").find_all("strong")[2].contents[0].strip("%"))
-
-print(num)
-
-
